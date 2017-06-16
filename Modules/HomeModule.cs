7d27b2c144f3fa1030a630891ac82eaa4b765cdf
YcleptInsan <nickmwise@gmail.com>
2017-06-16 08:16:29 -0700

    Initial files for Project. HomeModule, Views, Startup, project.json.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+
+namespace WorldTour
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+     };
